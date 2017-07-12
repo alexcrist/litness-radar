d7f478414626f43aa5ce7ff1290f1bd1ccc21597
Alex Crist <alexecrist@gmail.com>
2017-07-12 18:06:38 -0400

    Add front end

--- src/public/index.js
+++ src/public/index.js
@@ -1,1 +1,17 @@
-alert('test')
+$('.button-join').click(function () {
+ $.get('/join', function (res) {
+
+ });
+});
+
+$('.button-drink').click(function () {
+  $.get('/drink', function (res) {
+
+  });
+});
+
+$('.button-leave').click(function () {
+ $.get('/leave', function (res) {
+
+ });
+});
